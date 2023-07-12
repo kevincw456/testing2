b15d2b9b549bdf257737ccd25e3f3cfbc6b185fe
kevincw456 <119997828+kevincw456@users.noreply.github.com>
2023-07-12 11:22:01 +0800

    changeNEWWW

--- demo/settings.py
+++ demo/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+
+DATABASE_URL = "postgresql://postgres:0UcscXzEQXOPqUtCrigQ@containers-us-west-24.railway.app:5810/railway"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,16 +83,20 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': '0UcscXzEQXOPqUtCrigQ',
-        'HOST': 'containers-us-west-24.railway.app',
-        'PORT': '5810',
-    }
+   'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)     
 }
+
+
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'railway',
+#       'USER': 'postgres',
+#        'PASSWORD': '0UcscXzEQXOPqUtCrigQ',
+#        'HOST': 'containers-us-west-24.railway.app',
+#        'PORT': '5810',
+    
 
 
 # Password validation
